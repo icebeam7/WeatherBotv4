320680b8c2b1023461ea60c1e4578b74205af3db
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-11-11 13:21:03 +0100

    Revert "Azure Bot Services Configured"
    This reverts commit c6c00db35a071860fb557e81f6f00f4d4f8d6a9f.

--- WeatherBotv4/Helpers/Constants.cs
+++ WeatherBotv4/Helpers/Constants.cs
@@ -7,6 +7,6 @@
         public readonly static string LocationPatternLabel = "Location_PatternAny";
 
         public static string OpenWeatherMapURL = $"http://api.openweathermap.org/data/2.5/weather";
-        public readonly static string OpenWeatherMapKey = "beb91969bac55dabac2b7cc6a3fc5f88";
+        public readonly static string OpenWeatherMapKey = "tu-llave-de-openweathermap";
     }
 }
